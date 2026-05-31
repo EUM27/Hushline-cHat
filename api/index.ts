164cd7bf039ef84af1879f86e39511dfadf6a42b
EUM27 <263309219+EUM27@users.noreply.github.com>
2026-05-31 12:24:47 +0900

    fix: expose vercel api methods

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,9 @@
-export { default } from "../packages/server/src/vercel-app";
+import app from "../packages/server/src/vercel-app";
+
+function handle(request: Request): Response | Promise<Response> {
+  return app.fetch(request);
+}
+
+export const GET = handle;
+export const POST = handle;
+export const OPTIONS = handle;
